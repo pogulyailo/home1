0d3c8752b2576c8824d323a98155da13bc50fca1
igr <mixa_fc@ukr.net>
2017-09-06 01:06:51 +0300

    как я провел лето

---
+++ src/home1.java
@@ -0,0 +1,7 @@
+public class home1 {
+
+        public static void main(String[] args) {
+
+            System.out.println("Первых два месяца лета я провел в командировке, в августе был в отпуске");
+        }
+}
